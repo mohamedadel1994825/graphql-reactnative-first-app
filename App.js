a1a2038ce5a4c020a4c295f7d7a90cbaa0fe2211
mohamed adel <mohamedadel1994825@gmail.com>
2020-09-25 17:43:36 +0200

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import * as React from "react";
+import Home from "./src/Home";
+import CoffeePage from "./src/CoffeePage";
+import { View } from "react-native";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider, Query } from "react-apollo";
+const client = new ApolloClient({
+  uri: "https://yq42lj36m9.sse.codesandbox.io/",
+});
+import gql from "graphql-tag";
+const QUERY = gql`
+{
+    coffee {
+        beans {
+            key
+            name
+            price
+            blend
+            color
+            productImage
+        }
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+`;
+const Stack = createStackNavigator();
+const NavCont = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="CoffeePage" component={CoffeePage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+export default (App = () => {
+  return (
+    <ApolloProvider client={client}>
+      <Query query={QUERY}>
+         {({ loading, error, data }) => {
+             console.log("data1===================",data) 
+            if (loading || error) return <View />
+            return <View style={{ flex: 1 }}>
+            <NavCont /> 
+            </View>
+        }}
+      </Query>
+    </ApolloProvider>
+  );
 });
