5bba7bc45821d86b1bc81c980b08dc5bc51a2031
mohamed adel <mohamedadel1994825@gmail.com>
2020-09-27 09:58:12 +0200

    first commit

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import Home from "./src/Home";
 import CoffeePage from "./src/CoffeePage";
-import { View } from "react-native";
+import { View, Text, Image, StatusBar } from "react-native";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -11,19 +11,20 @@ const client = new ApolloClient({
   uri: "https://yq42lj36m9.sse.codesandbox.io/",
 });
 import gql from "graphql-tag";
+import { FlatList } from "react-native-gesture-handler";
 const QUERY = gql`
-{
+  {
     coffee {
-        beans {
-            key
-            name
-            price
-            blend
-            color
-            productImage
-        }
+      beans {
+        key
+        name
+        price
+        blend
+        color
+        productImage
+      }
     }
-}
+  }
 `;
 const Stack = createStackNavigator();
 const NavCont = () => {
@@ -40,12 +41,31 @@ export default (App = () => {
   return (
     <ApolloProvider client={client}>
       <Query query={QUERY}>
-         {({ loading, error, data }) => {
-             console.log("data1===================",data) 
-            if (loading || error) return <View />
-            return <View style={{ flex: 1 }}>
-            <NavCont /> 
-            </View>
+        {({ loading, error, data }) => {
+          console.log("data1===================", data);
+          if (loading || error) return <View />;
+          return (
+              // {/* <NavCont />  */}
+              <>
+                <FlatList
+                style={{flex:1,marginTop:20}}
+                  data={data.coffee.beans}
+                  keyExtractor={(item) => item.key + ""}
+                  renderItem={({ item, index }) => (
+                    <View style={{ width:'100%', backgroundColor:'green',marginBottom:20}}>
+                                            <Text style={{fontSize:30,color:'white',textAlign:'center'}}>hello</Text>
+                      <Text style={{fontSize:30,color:'white',textAlign:'center'}}>{item.name}</Text>
+                      <Text  style={{fontSize:30,color:'white',textAlign:'center'}}>{item.price}</Text>
+                      <Text  style={{fontSize:30,color:'white',textAlign:'center'}}>{item.blend}</Text>
+                      <Text  style={{fontSize:30,color:'white',textAlign:'center'}}>{item.color}</Text>
+                      <Image style={{width:300,height:100,alignSelf:'center'}}
+                      source={{uri:item.productImage}}
+                      />
+                    </View>
+                  )}
+                />
+                </>
+          );
         }}
       </Query>
     </ApolloProvider>
